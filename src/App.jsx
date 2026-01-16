d57359a926f6e9a01771355e53cd2efe913c471e
Kritik Patidar <kritikpatidar24@gmail.com>
2026-01-16 23:46:23 +0530

    Tailwind and vite setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
-
-function App() {
-
+export default function App() {
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen bg-green-600 flex items-center justify-center">
+      <h1 className="text-5xl font-extrabold text-white">
+        TAILWIND IS WORKING
+      </h1>
+    </div>
   )
 }
 
-export default App
+
+
