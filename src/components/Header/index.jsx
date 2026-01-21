8068aeeaa06baae479f44d8cd6f5739c61b51f55
Kritik Patidar <kritikpatidar24@gmail.com>
2026-01-21 23:32:01 +0530

    Added Search, Login/Register button in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,44 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Search from '../Search';
-import './index.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import Search from "../Search";
+import "./index.css";
+import Button from "@mui/material/Button";
 
 export const Header = () => {
   return (
     <header>
-        <div className='top-strip'>
-            <div className='container'>
-                <div className='top-header'>
-                    <div className='col1'>
-                        <p className='col1-heading-text'>Get up to 50% off new season styles, limited time only</p>
-                    </div>
-                    <div className="col2">
-                        <ul className='top-header-list-container'>
-                            <li className='top-header-list-item'>
-                                <Link to='/help-center' className='header-list-text'>Help-Center</Link>
-                            </li>
-                            <li className='top-header-list-item'>
-                                <Link to={'order-center'} className='header-list-text'>order-Center</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                
+      <div className="top-strip">
+        <div className="container">
+          <div className="top-header">
+            <div className="col1">
+              <p className="col1-heading-text">
+                Get up to 50% off new season styles, limited time only
+              </p>
             </div>
-        </div>
-        <div className="main-header">
-            <div className="container main-header-container">
-                <div className="main-col1">
-                    <Link to={"/"}><img src="/logo.svg" alt='image'width={222} height={44}/></Link>
-                </div>
-                <div className="main-col2">
-                    <Search/>
-                </div>
-                <div className="main-col3"></div>
+            <div className="col2">
+              <ul className="top-header-list-container">
+                <li className="top-header-list-item">
+                  <Link to="/help-center" className="header-list-text">
+                    Help-Center
+                  </Link>
+                </li>
+                <li className="top-header-list-item">
+                  <Link to={"order-center"} className="header-list-text">
+                    order-Center
+                  </Link>
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
+      </div>
+      <div className="main-header">
+        <div className="container main-header-container">
+          <div className="main-col1">
+            <Link to={"/"}>
+              <img src="/logo.svg" alt="image" width={222} height={44} />
+            </Link>
+          </div>
+          <div className="main-col2">
+            <Search />
+          </div>
+          <div className="main-col3">
+            <ul className="signupList">
+              <li className="signupListItem">
+                <Link to={"login"} className="link loginLinkStyle">Login</Link> | <Link to={"Register"} className="link loginLinkStyle">Register</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
+import Button from "@mui/material/Button";
+import { IoSearch } from "react-icons/io5";
 
 const Search = () => {
   return (
-    <div className='searchBox'>
-      <input type='text' placeholder='Search for products...' className='searchInput'/>
+    <div className="searchBox">
+      <input
+        type="text"
+        placeholder="Search for products..."
+        className="searchInput"
+      />
+      <Button
+        className="searchButton"
+        sx={{
+          width: 38,
+          height: 38,
+          minWidth: 38,
+          borderRadius: "50%",
+          color: '#363535'
+        }}
+      >
+        <IoSearch className="searchIconStyle"/>
+      </Button>
     </div>
-  )
-}
+  );
+};
 
 export default Search;
